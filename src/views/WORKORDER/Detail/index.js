7f518ba7b54ce43bc036150dcafe593984ab3615
vanhieuhoaiphu <vanhieuhoaiphu@gmail.com>
2023-02-08 21:48:59 +0700

    modify-position-work-order

--- src/views/WORKORDER/Detail/index.js
+++ src/views/WORKORDER/Detail/index.js
@@ -412,36 +412,43 @@ const WorkorderModal = () => {
 
   const handleChange = (e) => {
     const value = e.target.value;
-    let from_date = workorder.from_date;
-    let to_date = workorder.to_date;
-    if (e.target.name === 'to_date') {
-      if (workorder.from_date < value) {
-        setCheckChangeData({ ...checkChangeData, changeWorkOrder: true })
-        setWorkorder({
-          ...workorder,
-          to_date: value,
-          from_date: from_date,
-        });
-      }
-    } else if (e.target.name === 'from_date') {
-      if (value < workorder.to_date) {
-        setCheckChangeData({ ...checkChangeData, changeWorkOrder: true })
-        setWorkorder({
-          ...workorder,
-          to_date: to_date,
-          from_date: value,
-        });
-      }
-    } else {
       setCheckChangeData({ ...checkChangeData, changeWorkOrder: true })
       setWorkorder(pre => ({
         ...pre,
         [e.target.name]: value,
       }));
-    }
     updateDataDailyRequest(productList);
-
   };
+  const setToDate = (currentDate) =>{
+    var month = currentDate.getMonth()+1;
+    if (month < 10) month = "0" + month;
+    var dateOfMonth = currentDate.getDate();
+    if (dateOfMonth < 10) dateOfMonth = "0" + dateOfMonth;
+    var year = currentDate.getFullYear();
+    var value = year + "-" + month + "-" + dateOfMonth;
+      if (workorder.from_date < value) {
+        setCheckChangeData({ ...checkChangeData, changeWorkOrder: true })
+        setWorkorder({
+          ...workorder,
+          to_date: value,
+        });
+      }
+  }
+  const setFromDate = (currentDate) =>{
+    var month = currentDate.getMonth()+1;
+    if (month < 10) month = "0" + month;
+    var dateOfMonth = currentDate.getDate();
+    if (dateOfMonth < 10) dateOfMonth = "0" + dateOfMonth;
+    var year = currentDate.getFullYear();
+    var value = year + "-" + month + "-" + dateOfMonth;
+      if (value < workorder.to_date) {
+        setCheckChangeData({ ...checkChangeData, changeWorkOrder: true })
+        setWorkorder({
+          ...workorder,
+          from_date: value,
+        });
+      }
+  }
   const handleChangeHours = (e) => {
 
     const value = e.target.value;
@@ -705,7 +712,9 @@ const WorkorderModal = () => {
     if (to_date !== '' && from_date !== '') {
 
       for (var d = new Date(from_date); d <= new Date(to_date); d.setDate(d.getDate() + 1)) {
-        const day = d.getFullYear() + '-' + month[d.getMonth()] + '-' + d.getDate();
+        let Date =  d.getDate();
+        if (Date<10) Date='0'+Date
+        const day = d.getFullYear() + '-' + month[d.getMonth()] + '-' +Date;
         if (!selectedDocument) {
           date = [
             ...date,
@@ -796,7 +805,10 @@ const WorkorderModal = () => {
     let date = [];
     if (to_date !== '' && from_date !== '') {
       for (var d = new Date(from_date); d <= new Date(to_date); d.setDate(d.getDate() + 1)) {
-        const day = d.getFullYear() + '-' + month[d.getMonth()] + '-' + d.getDate();
+        let Date =  d.getDate();
+        if (Date<10) Date='0'+Date
+        const day = d.getFullYear() + '-' + month[d.getMonth()] + '-' +Date;
+  
         let data = productionDailyRequestList.find((x) => x.work_order_date === day);
         if (!data) {
           date = [
@@ -859,12 +871,12 @@ const WorkorderModal = () => {
         {' '}
         <TableRow key={index} sx={{ '&:last-child td, &:last-child th': { border: 0 } }} hover style={{ minWidth: 10, maxWidth: 10 }}>
           <TableCell align="left">{index + 1}</TableCell>
-          <TableCell align="left" style={{ maxWidth: 50, overflow: 'hidden', textOverflow: 'ellipsis' }}>
+          <TableCell align="left" style={{ maxWidth: 150,minWidth: 150, overflow: 'hidden', textOverflow: 'ellipsis' }}>
             <Tooltip title={item.customer_order_code}>
               <span> {item.customer_order_code}</span>
             </Tooltip>
           </TableCell>
-          <TableCell align="left" style={{ minWidth: 150, maxWidth: 150 }}>
+          <TableCell align="left" style={{ minWidth: 200, maxWidth: 200 }}>
             <Autocomplete
               value={item}
               size="small"
@@ -880,7 +892,7 @@ const WorkorderModal = () => {
 
           <TableCell align="left">{item.product_customer_code}</TableCell>
 
-          <TableCell align="left" style={{ maxWidth: 250, overflow: 'hidden', textOverflow: 'ellipsis' }}>
+          <TableCell align="left" style={{ maxWidth: 400,minWidth: 400, overflow: 'hidden', textOverflow: 'ellipsis' }}>
             <Tooltip title={item.product_name}>
               <span> {item.product_name}</span>
             </Tooltip>
@@ -1015,7 +1027,7 @@ const WorkorderModal = () => {
                                   />
                                 </Grid>
                                 <Grid item lg={3} md={3} xs={3}>
-                                  <span className={classes.tabItemLabelField}>Trạng thái: </span>
+                                  {/* <span className={classes.tabItemLabelField}>Trạng thái: </span>
                                   <TextField
                                     select
                                     fullWidth
@@ -1031,47 +1043,8 @@ const WorkorderModal = () => {
                                           {item.value}
                                         </MenuItem>
                                       ))}
-                                  </TextField>
-                                </Grid>
-                              </Grid>
-
-                              <Grid container className={classes.gridItemInfo} alignItems="center" spacing={1}>
-                                <Grid item lg={2} md={2} xs={2}>
-                                  <span className={classes.tabItemLabelField}>Thời gian bắt đầu:</span>
-                                  <TextField
-                                    fullWidth
-                                    type="date"
-                                    variant="outlined"
-                                    name="from_date"
-                                    value={workorder.from_date}
-                                    size="small"
-                                    onChange={handleChange}
-                                  />
-                                  {/* <DatePicker
-                                    // date={orderData.expected_deliver_date}
-                                    // onChange={(date) => setOrderData({ ...orderData, expected_deliver_date: date })}
-                                  /> */}
-                                </Grid>
-
-
-                                <Grid item lg={2} md={2} xs={2}>
-                                  <span className={classes.tabItemLabelField}>Thời gian kết thúc:</span>
-                                  <TextField
-                                    fullWidth
-                                    type="date"
-                                    variant="outlined"
-                                    name="to_date"
-                                    value={workorder.to_date}
-                                    size="small"
-                                    onChange={handleChange}
-                                  />
-                                  {/* <DatePicker
-                                    // date={orderData.expected_deliver_date}
-                                    // onChange={(date) => setOrderData({ ...orderData, expected_deliver_date: date })}
-                                  /> */}
-                                </Grid>
-                                <Grid item lg={3} md={3} xs={3}>
-                                  <span className={classes.tabItemLabelField}>Xưởng: </span>
+                                  </TextField> */}
+                                   <span className={classes.tabItemLabelField}>Xưởng: </span>
                                   <TextField
                                     select
                                     fullWidth
@@ -1089,8 +1062,66 @@ const WorkorderModal = () => {
                                       ))}
                                   </TextField>
                                 </Grid>
+                              </Grid>
 
-                                <Grid item lg={2} md={2} xs={2}>
+                              <Grid container className={classes.gridItemInfo} alignItems="center" spacing={1}>
+                                <Grid item lg={3} md={3} xs={3}>
+                                  <span className={classes.tabItemLabelField}>Thời gian bắt đầu:</span>
+                                  {/* <TextField
+                                    fullWidth
+                                    type="date"
+                                    variant="outlined"
+                                    name="from_date"
+                                    format={'dd/mm/yyyy'}
+                                    value={workorder.from_date}
+                                    size="small"
+                                    onChange={handleChange}
+                                  /> */}
+                                  <DatePicker
+                                    date={workorder.from_date}
+                                    
+                                    onChange={(date) => setFromDate(date)}
+                                  />
+                                </Grid>
+
+
+                                <Grid item lg={3} md={3} xs={3}>
+                                  <span className={classes.tabItemLabelField}>Thời gian kết thúc:</span>
+                                  {/* <TextField
+                                    fullWidth
+                                    type="date"
+                                    variant="outlined"
+                                    name="to_date"
+                                    value={workorder.to_date}
+                                    size="small"
+                                    onChange={handleChange}
+                                  /> */}
+                                   <DatePicker
+                                    date={workorder.to_date}
+                                    onChange={(date) => setToDate(date)}
+                                  />
+                                </Grid>
+                                {/* <Grid item lg={3} md={3} xs={3}>
+                                  <span className={classes.tabItemLabelField}>Xưởng: </span>
+                                  <TextField
+                                    select
+                                    fullWidth
+                                    id="outlined-size-small"
+                                    variant="outlined"
+                                    size="small"
+                                    value={workorder?.workshop_id}
+                                    onChange={(event) => setWorkorder({ ...workorder, workshop_id: event.target.value })}
+                                  >
+                                    {workshopList &&
+                                      workshopList.map((item) => (
+                                        <MenuItem key={item.Key} value={item.Key}>
+                                          {item.Value}
+                                        </MenuItem>
+                                      ))}
+                                  </TextField>
+                                </Grid> */}
+
+                                <Grid item lg={3} md={3} xs={3}>
                                   <span className={classes.tabItemLabelField}>Kho vật tư:</span>
                                   <TextField
                                     select
