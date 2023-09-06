b44a802408f193e5122683b24e4e9ef14282241d
dangquangtai <quangtai26801@gmail.com>
2023-09-06 22:09:15 +0700

    update_ui_6_9_2023

--- src/views/Report/Detail/ViewDataTable/row.table..js
+++ src/views/Report/Detail/ViewDataTable/row.table..js
@@ -294,10 +294,11 @@ const Row = (props) => {
       );
     }
     if (reportType === 'BAO_CAO_THUA_THIEU_VAT_TU_NHA_CUNG_CAP') {
-      const { difference_quantity_in_piece, part_code, part_name, unit_name, supplier_name } = row;
+      const { category_name, difference_quantity_in_piece, part_code, part_name, unit_name, supplier_name } = row;
       return (
         <>
           <TableCell align="left">{index + 1}</TableCell>
+          <TableCell align="left">{category_name ? category_name : ''}</TableCell>
           <TableCell align="left">{supplier_name ? supplier_name : ''}</TableCell>
           <TableCell align="left">{part_code ? part_code : ''}</TableCell>
           <TableCell align="left">{part_name ? part_name : ''}</TableCell>
